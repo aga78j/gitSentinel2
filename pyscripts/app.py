458bdd17c2afd6cf885602a247d8ef5a7b0f036c
Antonio Miguel García Abellán <amgarcia278@alu.ucam.edu>
2021-12-30 21:57:07 +0100

    30/12/2021

--- pyscripts/app.py
+++ pyscripts/app.py
@@ -4,14 +4,34 @@ api = SentinelAPI('agarciabellan', '@Gar1983', 'https://apihub.copernicus.eu/api
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def main():
+    pass
+
 @app.route('/download', methods=['POST'])
 def download():
-    footprint = geojson_to_wkt(read_geojson(request.form.get('geoJSON')))
+    file = request.files.get("geoJSON")
+
+    if file.filename != "":
+        file.save("C:/Users/agarc/Desktop/TFG/gitSentinel2/archivos_geoJSON/json.geojson")
+
+    footprint = geojson_to_wkt(read_geojson("C:/Users/agarc/Desktop/TFG/gitSentinel2/archivos_geoJSON/json.geojson"))
     products = api.query(footprint,
                      date=(request.form.get('date_init').replace('-', ''), request.form.get('date_end').replace('-', '')),
                      platformname=request.form.get('platform'),
                      producttype = request.form.get('product'),
                      cloudcoverpercentage=(0, int(request.form.get("cloud"))))
+                     
     #print(request.form.get('date_init').replace('-', ''), request.form.get('date_end').replace('-', ''), request.form.get('platform'), request.form.get('product'))
     api.download_all(products, directory_path='C:/Users/agarc/Desktop/TFG/gitSentinel2/fotos')
     return "terminado"
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="@Gar1983"
+)
+
+print(mydb)
